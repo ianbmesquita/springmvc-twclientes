3b1fd2caab6405dce8e19680e07a5d8c6ddc75dd
ianbmesquita <ianmesquitadb@gmail.com>
2021-12-10 12:24:17 -0300

    feat: Creating the customer module - customer registration.

--- src/main/java/br/com/treinaweb/twclientes/controller/ClienteController.java
+++ src/main/java/br/com/treinaweb/twclientes/controller/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -36,6 +37,21 @@ public class ClienteController {
         modelAndView.addObject("cliente", cliente);
 
         return modelAndView;
+    }
+
+    @GetMapping("/cadastrar")
+    public ModelAndView cadastrar() {
+        ModelAndView modelAndView = new ModelAndView("cliente/cadastrar.html");
+        modelAndView.addObject("cliente", new Cliente());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public ModelAndView cadastrar(Cliente cliente) {
+        clienteRepository.save(cliente);
+
+        return new ModelAndView("redirect:/cliente");
     }
 
     @GetMapping("/{id}/excluir")

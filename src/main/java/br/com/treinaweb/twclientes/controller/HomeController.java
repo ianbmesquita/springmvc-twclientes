9e448ae9f6420f859f2019a61768d0c1d382ebd4
ianbmesquita <ianmesquitadb@gmail.com>
2021-12-10 09:28:54 -0300

    feat: Creating the client module - model and repository.

--- src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
+++ src/main/java/br/com/treinaweb/twclientes/controller/HomeController.java
@@ -1,9 +1,13 @@
 package br.com.treinaweb.twclientes.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 // import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -13,6 +17,32 @@ public class HomeController {
     @GetMapping("/")
     private String home() {
         return "home";
+    }
+
+    @GetMapping("/mensagem")
+    private String mensagem(Model model) {
+        model.addAttribute("mensagem", "Bem vindos!!!");
+        
+        return "mensagem";
+    }
+
+    // @GetMapping("/saudacao/{nome}")
+    // private ModelAndView saudacao(@PathVariable String nome) {
+    //     ModelAndView modelAndView = new ModelAndView();
+    //     modelAndView.setViewName("saudacao");
+    //     modelAndView.addObject("nome", nome);
+
+    //     return modelAndView;
+    // }
+
+    //treinaweb.com.br/saudacao?nome=Ian
+    @GetMapping("/saudacao")
+    private ModelAndView saudacao(@RequestParam(required = false, defaultValue = "Treinaweb") String nome) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("saudacao");
+        modelAndView.addObject("nome", nome);
+
+        return modelAndView;
     }
     
 }
